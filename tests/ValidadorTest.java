86fe67f69a40cf4a32479848986e4fbc5fe30a8a
FernandoBersellini <fernandobersellini@outlook.com.br>
2025-09-17 21:12:58 -0300

    :test_tube: test: Testes para verificar CPF´s válidos com máscara e sem máscara

--- tests/ValidadorTest.java
+++ tests/ValidadorTest.java
@@ -5,6 +5,16 @@ import org.junit.jupiter.api.Test;
 
 public class ValidadorTest {
     @Test
+    public void validarCPFcomMascara() {
+        assertEquals(true, Validador.validarCPF("529.982.247-25"));
+    }
+
+    @Test
+    public void validarCPFsemMascara() {
+        assertEquals(true, Validador.validarCPF("52998224725"));
+    }
+
+    @Test
     public void validarCPFvazio(){
         assertEquals(false, Validador.validarCPF(""));
     }
