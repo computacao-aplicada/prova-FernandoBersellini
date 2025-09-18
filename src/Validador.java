fbc22bc9fccae92846041b508586e00a1a6103a3
FernandoBersellini <fernandobersellini@outlook.com.br>
2025-09-17 21:45:45 -0300

    :recycle: refactor: Código do validador refatorado

--- src/Validador.java
+++ src/Validador.java
@@ -2,29 +2,50 @@ public class Validador {
 
     public static boolean validarCPF(String cpf) {
         if (cpf == null || cpf.trim().isEmpty()) return false;
-        String limpo = cpf.trim().replaceAll("[.-]", "");
-        if (!limpo.matches("\\d{11}")) return false;
-        if (limpo.chars().distinct().count() == 1) return false;
-        if (checarDigitos(limpo)) {
+
+        String cpflimpo = sanitizarCPF(cpf);
+
+        if (checarFormato(cpflimpo)) return false;
+
+        if (checarDigitos(cpflimpo)) {
             return true;
         } else {
             return false;
         }
     }
 
-    private static boolean checarDigitos(String cpf) {
-        int[] d = cpf.chars().map(c -> c - '0').toArray();
-        int s1 = 0;
-        for (int i = 0; i < 9; i++) s1 += d[i] * (10 - i);
-        int r1 = s1 % 11;
-        int dv1 = (r1 < 2) ? 0 : 11 - r1;
-        if (d[9] != dv1) return false;
+    //Verifica se o CPF contém 11 dígitos apos a sanitizacao
+    private static boolean checarFormato(String cpflimpo) {
+        if (!cpflimpo.matches("\\d{11}")) return true;
 
-        int s2 = 0;
-        for (int i = 0; i < 10; i++) s2 += d[i] * (11 - i);
-        int r2 = s2 % 11;
+        return cpflimpo.chars().distinct().count() == 1;
+    }
+
+    //Este método retira os caracteres não numéricos do CPF
+    private static String sanitizarCPF(String cpf) {
+        String cpflimpo = cpf.trim().replaceAll("[.-]", "");
+        return cpflimpo;
+    }
+
+
+    //Verifica se os digitos verificadores batem com o 10º e 11º digito do CPF
+    private static boolean checarDigitos(String cpf) {
+        int[] digitosDoCpf = cpf.chars().map(c -> c - '0').toArray();
+
+
+        int sanitizado1 = 0;
+        for (int i = 0; i < 9; i++) sanitizado1 += digitosDoCpf[i] * (10 - i);
+
+        int r1 = sanitizado1 % 11;
+        int digitoVerificador1 = (r1 < 2) ? 0 : 11 - r1;
+
+        if (digitosDoCpf[9] != digitoVerificador1) return false;
+
+        int sanitizado2 = 0;
+        for (int i = 0; i < 10; i++) sanitizado2 += digitosDoCpf[i] * (11 - i);
+        int r2 = sanitizado2 % 11;
         int dv2 = (r2 < 2) ? 0 : 11 - r2;
-        return d[10] == dv2;
+        return digitosDoCpf[10] == dv2;
     }
 }
 
